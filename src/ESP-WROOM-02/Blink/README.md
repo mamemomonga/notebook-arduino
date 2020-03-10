4d017ed697b3e0dabd1b44509ddec620689ce009
mamemomonga <mamemomonga@gmail.com>
2020-03-11 08:05:42 +0900

    LチカとHello

---
+++ src/ESP-WROOM-02/Blink/README.md
@@ -0,0 +1,5 @@
+# Blink
+
+* [配線](../../Arduino-CLI/ESP-WROOM-02.md)
+
+* Lチカです。IO4にLEDを接続します。
--- src/ESP-WROOM-02/Hello/Hello.ino
+++ src/ESP-WROOM-02/Hello/Hello.ino
@@ -1,9 +1,13 @@
 void setup(void) {
     pinMode(4, OUTPUT);
+	Serial.begin(115200);
+	Serial.println();
+	Serial.println("Serial Ready");
 }
 
 void loop(void) {
     digitalWrite(4, HIGH);
+	Serial.println("Hello");
     delay(500);
     digitalWrite(4, LOW);
     delay(500);
