643d2ce7adab95b2de88f1c08844b19adc3362b3
show <mamemomonga@gmail.com>
2020-03-17 01:17:08 +0900

    バージョンを追従

--- src/ESP-WROOM-02/WiFiApp/bin/build.sh
+++ src/ESP-WROOM-02/WiFiApp/bin/build.sh
@@ -3,17 +3,25 @@ set -eu
 BASEDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && cd .. && pwd )"
 CURDIR=$(pwd)
 
+# ---------------------------------------------------
+
 AR_DATA=$(arduino-cli config dump --format json | jq -r '.directories.data')
 AR_PORT=$(cat ../serial_port)
 AR_BAUD=921600
 AR_EESZ="4M1M"
+AR_FQBN="esp8266:esp8266:generic"
+AR_FQBN_WOPT="$AR_FQBN:baud=$AR_BAUD,eesz=$AR_EESZ"
 
-MKSPIFFS="$AR_DATA/packages/esp8266/tools/mkspiffs/2.5.0-4-b40a506/mkspiffs"
- PYTHON3="$AR_DATA/packages/esp8266/tools/python3/3.7.2-post1/python3"
- ESPTOOL="$AR_DATA/packages/esp8266/hardware/esp8266/2.6.3/tools/esptool/esptool.py"
-PYSERIAL="$AR_DATA/packages/esp8266/hardware/esp8266/2.6.3/tools/pyserial"
+AR_TOOLS=$(arduino-cli board details --format json $AR_FQBN | jq -r '.required_tools[]')
 
-AR_FQBN="esp8266:esp8266:generic:baud=$AR_BAUD,eesz=$AR_EESZ"
+ESP8266_VERSION=$( arduino-cli core search esp8266 | sed '1d' | sed '$d' | awk '{ print $2 }' )
+MKSPIFFS_VERSION=$( echo "$AR_TOOLS" | jq -r '.| select(.name == "mkspiffs") | .version' )
+PYTHON3_VERSION=$(  echo "$AR_TOOLS" | jq -r '.| select(.name == "python3") | .version' )
+
+MKSPIFFS="$AR_DATA/packages/esp8266/tools/mkspiffs/$MKSPIFFS_VERSION/mkspiffs"
+ PYTHON3="$AR_DATA/packages/esp8266/tools/python3/$PYTHON3_VERSION/python3"
+ ESPTOOL="$AR_DATA/packages/esp8266/hardware/esp8266/$ESP8266_VERSION/tools/esptool/esptool.py"
+PYSERIAL="$AR_DATA/packages/esp8266/hardware/esp8266/$ESP8266_VERSION/tools/pyserial"
 
 SPIFFS_FILE="$BASEDIR/var/spiffs.bin"
 
@@ -26,10 +34,12 @@ echo "  SPIFFS_BLOCKSIZE: $SPIFFS_BLOCKSIZE"
 echo "  SPIFFS_END:       $SPIFFS_END"
 echo "  SPIFFS_START:     $SPIFFS_START"
 
+# ---------------------------------------------------
+
 echo
-echo "*** コンパイル開始 ***"
+echo "*** コンパイル ***"
 cd "$BASEDIR/.."
-arduino-cli compile --fqbn $AR_FQBN $BASEDIR
+arduino-cli compile --fqbn $AR_FQBN_WOPT $BASEDIR
 cd $CURDIR
 
 echo
@@ -49,20 +59,16 @@ echo
 echo "*** スケッチアップロード ***"
 echo "  プログラムモードにしてください"
 cd "$BASEDIR/.."
-set -x
-arduino-cli upload -p $AR_PORT --fqbn $AR_FQBN $BASEDIR
-set +x
+arduino-cli upload -p $AR_PORT --fqbn $AR_FQBN_WOPT $BASEDIR
 cd $CURDIR
 
 echo
 echo "*** SPIFFS アップロード ***"
 echo "  プログラムモードにしてください"
-set -x
 PYTHONPATH="$PYSERIAL" $PYTHON3 $ESPTOOL \
 	--baud 921600 \
 	--port $(cat ../serial_port) \
 	write_flash $SPIFFS_START $SPIFFS_FILE -u
-set +x
 
 echo
 echo "*** シリアル接続開始 ***"
